fb7d1cd1df808a89d4f935b6baacd3bad1145f61
matijamicunovic629 <micunovicmatija629@gmail.com>
2025-02-10 14:04:55 +0100

    [Chore] fix vite react buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,13 @@ export default defineConfig({
     esbuildOptions: {
       define: {
         global: 'globalThis'
-      }
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
     }
   },
   server: {
