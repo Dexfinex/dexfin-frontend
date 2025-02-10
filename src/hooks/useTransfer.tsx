6f6e2882e92cfaa13b472e3e0fbc4cc220960c57
Puffer <devpuffer0807@gmail.com>
2025-02-10 20:50:22 +0900

    [Fix] disable to fetch transfer if wallet not connect

--- src/hooks/useTransfer.tsx
+++ src/hooks/useTransfer.tsx
@@ -11,9 +11,9 @@ export const useEvmWalletTransfer = () => {
 
     const { chainId: connectedChainId, address: connectedAddress } = useContext(Web3AuthContext);
 
-    const activeChainId = connectedChainId + "";
-    // const activeWalletAddress = connectedAddress;
-    const activeWalletAddress = "0x2b5634c42055806a59e9107ed44d43c426e58258";
+    const activeChainId = connectedChainId;
+    const activeWalletAddress = connectedAddress;
+    // const activeWalletAddress = "0x2b5634c42055806a59e9107ed44d43c426e58258";
 
     const enabled = !!activeChainId && !!activeWalletAddress;
     const fetchTransfer = useCallback(async () => {
