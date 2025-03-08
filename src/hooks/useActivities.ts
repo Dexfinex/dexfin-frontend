db7e9f0ae99af2adf03ed08bb74cbe947d51d189
Puffer <devpuffer0807@gmail.com>
2025-03-08 23:50:24 +0900

    [Feat] add activities store in wallet

---
+++ src/hooks/useActivities.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { dexfinv3Service, } from "../services/dexfin.service.ts";
+import useActivitiesStore from "../store/useActivitiesStore.ts";
+import { WalletActivityType } from "../types/dexfinv3.type.ts";
+
+export const useActivities = ({
+    evmAddress,
+    solanaAddress,
+}: {
+    evmAddress: string;
+    solanaAddress: string;
+}) => {
+    const fetchActivities = useCallback(async () => {
+        if (!evmAddress && !solanaAddress) {
+            return [];
+        }
+        const data = await dexfinv3Service.getAllActivities(evmAddress, solanaAddress);
+
+        if (data) {
+            return data;
+        }
+
+        return [];
+    }, [evmAddress, solanaAddress]);
+
+    const { isLoading, refetch, data } = useQuery<WalletActivityType[]>({
+        queryKey: ["activities", evmAddress, solanaAddress],
+        queryFn: fetchActivities,
+    })
+
+    useEffect(() => {
+        if (data) {
+            useActivitiesStore.getState().setActivities(data);
+        }
+    }, [data]);
+
+    return {
+        isLoading,
+        refetch,
+        data,
+    };
+};
