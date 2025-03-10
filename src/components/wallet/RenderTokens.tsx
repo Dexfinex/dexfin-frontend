939fb084b8dab5bd2390ce57e463c3cfcf19a976
Puffer <devpuffer0807@gmail.com>
2025-03-11 02:40:58 +0900

    [Refactor] optimize code in wallet drawer

---
+++ src/components/wallet/RenderTokens.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Skeleton } from '@chakra-ui/react';
+
+import { formatNumberByFrac } from "../../utils/common.util";
+import { formatUsdValue, } from '../../lib/wallet';
+import { TokenChainIcon } from "../swap/components/TokenIcon";
+import { useWalletBalance } from "../../hooks/useBalance";
+import useTokenBalanceStore, { TokenBalance } from "../../store/useTokenBalanceStore";
+
+
+interface RenderTokensProps {
+    handleAsset: (token: TokenBalance) => void
+}
+
+const RenderTokens: React.FC<RenderTokensProps> = ({ handleAsset }) => {
+    const { isLoading: isLoadingBalance } = useWalletBalance();
+    const { tokenBalances } = useTokenBalanceStore();
+
+    return (
+        <div className="flex-1 space-y-2 mt-4 sm:mt-5 overflow-y-auto ai-chat-scrollbar sm:max-h-[calc(100vh-350px)] max-h-[calc(100vh-290px)] mx-4">
+            {
+                isLoadingBalance ?
+                    <Skeleton startColor="#444" endColor="#1d2837" w={'100%'} h={'4rem'}></Skeleton>
+                    : tokenBalances.map((token, index) => (
+                        <button
+                            key={token.chain + token.symbol + index}
+                            className="flex w-full items-center justify-between p-3 bg-white/5 hover:bg-white/10 rounded-lg transition-colors"
+                            onClick={() => handleAsset(token)}
+                        >
+                            <div className="flex items-center gap-3">
+                                <TokenChainIcon src={token.logo} alt={token.name} size={"lg"} chainId={Number(token.chain)} />
+                                <div className='flex flex-col justify-start items-start'>
+                                    <div className="font-medium text-sm sm:text-md">{token.symbol}</div>
+                                    <div className="text-xs sm:text-sm text-white/60">
+                                        {`${formatNumberByFrac(token.balance, 5)} ${token.symbol}`}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="text-right text-sm md:text-md">
+                                <span>{formatUsdValue(token.usdValue)}</span>
+                            </div>
+                        </button>
+                    ))
+            }
+        </div>
+    )
+}
+
+export default RenderTokens;
