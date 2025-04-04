524f13e557d9cafc3093d3de2436a8115767880e
Itsolution1014 <itsolution1014@gmail.com>
2025-04-04 12:28:17 +0300

    [feat]: add withdraw action UI without brian in agent

--- src/components/agent/AIAgentModal.tsx
+++ src/components/agent/AIAgentModal.tsx
@@ -39,6 +39,7 @@ import useTokenBalanceStore from '../../store/useTokenBalanceStore.ts';
 import { DepositProcess } from './components/EVM/DepositProcess.tsx';
 import { EVMDepositProcess } from './components/EVM/EVMDepositProcess.tsx';
 import { WithdrawProcess } from './components/EVM/WithdrawProcess.tsx';
+import { EVMWithdrawProcess } from './components/EVM/EVMWithdrawProcess.tsx';
 import { BorrowProcess } from './components/EVM/BorrowProcess.tsx';
 import { RepayProcess } from './components/EVM/RepayProcess.tsx';
 import { ENSRegisterProcess } from './components/EVM/ENSRegisterProcess.tsx';
@@ -81,6 +82,7 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
   const [showDepositProcess, setShowDepositProcess] = useState(false);
   const [showEVMDepositProcess, setShowEVMDepositProcess] = useState(false);
   const [showWithdrawProcess, setShowWithdrawProcess] = useState(false);
+  const [showEVMWithdrawProcess, setShowEVMWithdrawProcess] = useState(false);
   const [showBorrowProcess, setShowBorrowProcess] = useState(false);
   const [showRepayProcess, setShowRepayProcess] = useState(false);
   const [showENSRegisterProcess, setShowENSRegisterProcess] = useState(false);
@@ -115,6 +117,7 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
     setShowDepositProcess(false);
     setShowEVMDepositProcess(false);
     setShowWithdrawProcess(false);
+    setShowEVMWithdrawProcess(false);
     setShowBorrowProcess(false);
     setShowRepayProcess(false);
     setShowENSRegisterProcess(false);
@@ -358,13 +361,14 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
         brianData: brianTransactionData.data,
         type: brianTransactionData.type
       }
-    } else if (sol_response && sol_response.type == "withdraw_evm") {
-      const brianTransactionData = await brianService.getBrianTransactionData(message, address, chainId);
-      return {
-        text: brianTransactionData.message,
-        brianData: brianTransactionData.data,
-        type: brianTransactionData.type
-      }
+    } else if (sol_response && sol_response.type == "withdraw_evm" && sol_response.args.chainName != "solana") {
+      return sol_response;
+      // const brianTransactionData = await brianService.getBrianTransactionData(message, address, chainId);
+      // return {
+      //   text: brianTransactionData.message,
+      //   brianData: brianTransactionData.data,
+      //   type: brianTransactionData.type
+      // }
     } else if (sol_response && sol_response.type == "renew_ens") {
       const brianTransactionData = await brianService.getBrianTransactionData(message, address, chainId);
       return {
@@ -896,7 +900,7 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
           const token = tokenBalances.find(item => item.symbol.toLowerCase() === response.args.inputSymbol.toLowerCase() && item.network?.id === fromNetwork.id);
 
           if (token && token.balance > response.args.amount) {
-            
+
             const toAddress = depositAddress(token.address, fromNetwork.chainId, response.args.protocolName);
 
             if (toAddress) {
@@ -943,7 +947,68 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
               response = { text: 'Missing mandatory parameter(s) in the prompt: address. Please rewrite the entire prompt.' }
             }
           } else {
-            response = { text: `deposit ${response.args.amount} ${response.args.inputSymbol} into ${response.args.protocolName} on ${fromNetwork.id}`, insufficient: 'Insufficient balance to perform the transaction.' };            
+            response = { text: `deposit ${response.args.amount} ${response.args.inputSymbol} into ${response.args.protocolName} on ${fromNetwork.id}`, insufficient: 'Insufficient balance to perform the transaction.' };
+          }
+        } else if (response.type == "withdraw_evm") {
+          resetProcessStates();
+          console.log(response);
+          const fromNetwork = mapChainName2Network[response.args.chainName];
+
+          if (chainId != fromNetwork.chainId) {
+            await switchChain(fromNetwork.chainId);
+          }
+
+          const token = tokenBalances.find(item => item.symbol.toLowerCase() === response.args.inputSymbol.toLowerCase() && item.network?.id === fromNetwork.id);
+
+          if (token && token.balance > response.args.amount) {
+
+            const toAddress = depositAddress(token.address, fromNetwork.chainId, response.args.protocolName);
+
+            if (toAddress) {
+              if (token.symbol == 'POL') {
+                setToToken({
+                  symbol: token.symbol,
+                  name: token.name,
+                  address: mapChainId2NativeAddress[Number(token.chain)],
+                  chainId: Number(token.chain),
+                  decimals: token.decimals,
+                  logoURI: token.logo,
+                  priceUSD: token.usdPrice
+                });
+              } else {
+                setToToken({
+                  symbol: token.symbol,
+                  name: token.name,
+                  address: token.address,
+                  chainId: Number(token.chain),
+                  decimals: token.decimals,
+                  logoURI: token.logo,
+                  priceUSD: token.usdPrice
+                });
+              }
+
+              setProtocol({
+                key: "",
+                logoURI: "",
+                name: response.args.protocolName,
+              });
+              setFromToken({
+                symbol: '',
+                name: '',
+                address: toAddress,
+                chainId: Number(token.chain),
+                decimals: 0,
+                logoURI: '',
+                priceUSD: 0,
+              });
+
+              setFromAmount(response.args.amount);
+              setShowEVMWithdrawProcess(true);
+            } else {
+              response = { text: 'Missing mandatory parameter(s) in the prompt: address. Please rewrite the entire prompt.' }
+            }
+          } else {
+            response = { text: `deposit ${response.args.amount} ${response.args.inputSymbol} into ${response.args.protocolName} on ${fromNetwork.id}`, insufficient: 'Insufficient balance to perform the transaction.' };
           }
         }
 
@@ -1191,6 +1256,12 @@ export default function AIAgentModal({ isOpen, widgetCommand, onClose }: AIAgent
           <EVMDepositProcess steps={steps} fromAmount={fromAmount} toToken={toToken} fromToken={fromToken} protocol={protocol}
             onClose={() => {
               setShowEVMDepositProcess(false);
+              setMessages([]);
+            }} />
+        ) : showEVMWithdrawProcess && fromToken && toToken ? (
+          <EVMWithdrawProcess steps={steps} receiver={receiver} fromAmount={fromAmount} toToken={toToken} fromToken={fromToken} protocol={protocol}
+            onClose={() => {
+              setShowEVMWithdrawProcess(false);
               setMessages([]);
             }} />
         ) :
